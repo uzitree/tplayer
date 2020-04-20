33c0cd7bd5e609910a4aa464379364169854c628
tulies <346461062@qq.com>
2020-04-20 11:18:52 +0800

    empty

--- README.md
+++
@@ -1,6 +0,0 @@
-# multi-view-player
-多视角播放器
-
-
-
-// https://www.cnblogs.com/goloving/p/8657205.html
--- example/assets/js/index.js
+++
@@ -1,5 +0,0 @@
-
-var mvPlayer = new MVPlayer('mvplayer',{
-  src:'assets/video/kungfu-free-30s-l42.mp4'
-})
-console.log(mvPlayer)
